1d1ea30bba62ed9c9d55da6613d828a658ec2a39
White Red <rednblkx@protonmail.com>
2026-02-19 17:56:00 +0200

    refactor: don't overstep esphome operation

--- components/homekit/esp_hap_core/src/esp_hap_mdns.c
+++ components/homekit/esp_hap_core/src/esp_hap_mdns.c
@@ -64,22 +64,10 @@ int hap_mdns_serv_stop(hap_mdns_handle_t *handle)
 
 int hap_mdns_init()
 {
-    int ret = HAP_SUCCESS;
-    if (!mdns_init_done) {
-        ret = mdns_init();
-        if (ret == ESP_OK) {
-            mdns_hostname_set("MyHost");
-            mdns_init_done = true;
-            ESP_MFI_DEBUG(ESP_MFI_DEBUG_INFO, "mDNS initialised");
-            return HAP_SUCCESS;
-        }
-    }
-    return HAP_FAIL;
+    return HAP_SUCCESS;
 }
 
 int hap_mdns_deinit()
 {
-    mdns_free();
-    mdns_init_done = false;
     return HAP_SUCCESS;
 }
